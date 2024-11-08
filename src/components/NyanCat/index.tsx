8deef0cf20de29e6f1401c438a4de013190cc250
RC Maples <rc@rcmaples.io>
2024-11-08 17:18:25 -0500

    nyancat component

---
+++ src/components/NyanCat/index.tsx
@@ -0,0 +1,21 @@
+import './styles.scss'
+
+export const NyanCat = () => {
+  return (
+    <div className="overlay">
+      <div className="rainbow">
+        <div className="sprite" />
+      </div>
+      <div className="cat">
+        <div className="tail">
+          <div className="sprite" />
+        </div>
+        <div className="feet">
+          <div className="sprite" />
+        </div>
+        <div className="poptart" />
+        <div className="head" />
+      </div>
+    </div>
+  )
+}
