a24dda412801b97635fffcc01922db8e97adbc6b
RC Maples <rc@rcmaples.io>
2024-11-14 21:29:07 -0500

    working via yalc

--- src/components/NyanCat/styles.tsx
+++ src/components/NyanCat/styles.tsx
@@ -1,113 +1,106 @@
 import {createGlobalStyle} from 'styled-components'
 
 const NyanCatStyles = createGlobalStyle`
-body {
-  overflow: hidden;
-  margin: 0;
-}
-
-.button--no-display {
-  padding: 0;
-  border: 0;
-  background: none;
-  box-shadow: none;
-  outline: none;
-  width: 36px;
-  height: 24px;
-}
-
-.cat,
-.poptart,
-.head,
-.feet,
-.tail,
-.rainbow,
-.sprite,
-.stars li,
-.stars li i {
-  position: absolute;
-  background-repeat: no-repeat;
-  animation-iteration-count: infinite;
-  animation-timing-function: linear;
-  animation-timing-function: step-end;
-}
-ul,
-li {
-  margin: 0;
-  padding: 0;
-  list-style: none;
-}
-
-@keyframes flyIn {
-  0% {
-    left: -150px;
+  body {
+    overflow: hidden;
+    margin: 0;
   }
-  100% {
+
+  .button--no-display {
+    padding: 0;
+    border: 0;
+    background: none;
+    box-shadow: none;
+    outline: none;
+    width: 36px;
+    height: 24px;
+  }
+
+  .cat--reverse {
+    transform: scaleX(-1) !important;
+  }
+
+  .cat-minimal {
+    left: 2% !important;
+    transform: scaleX(1);
+    transition: left 1500ms ease-in 1200ms;
+  }
+
+  .no-rainbow {
+    opacity: 0 !important;
+    transition: opacity 100ms ease-in !important;
+  }
+
+  .cat,
+  .poptart,
+  .head,
+  .feet,
+  .tail,
+  .rainbow,
+  .sprite {
+    position: absolute;
+    background-repeat: no-repeat;
+    animation-iteration-count: infinite;
+    animation-timing-function: step-end;
+  }
+
+
+  .overlay {
+    transform: scaleX(1);
+    position: absolute;
     left: 338px;
+    top: 38px;
+    z-index: 100;
+    transition:left 1500ms ease-in-out, transform 300ms ease;
+    animation: allow-discrete;
+    margin-bottom: 5px;
   }
-}
 
-.overlay {
-  position: absolute;
-  left: 338px;
-  top: 38px;
-  z-index: 100;
-  -webkit-transition: left 5s ease-in-out;
-  -moz-transition: left 5s ease-in-out;
-  -o-transition: left 5s ease-in-out;
-  transition: left 5s ease-in-out;
-  margin-bottom: 5px;
-}
+  .cat-left {
+    left: 338px !important;
 
-.cat-minimal {
-  left: 2% !important;
-}
-
-.cat-left {
-  left: 338px !important;
-  
-  @media screen and (max-width: 600px) {
-    left: 25% !important;
+    @media screen and (max-width: 600px) {
+      left: 25% !important;
+    }
   }
-}
 
-.cat-middle {
-  left: 50% !important;
-}
-
-.cat-right {
-  left: 80% !important;
-}
-
-.overlay--quarter {
-  left: -12vw !important;
-}
-
-.cat {
-  /*top: 50%;*/
-  /*left: 50%;*/
-  /*margin-left: -10px;*/
-  /*margin-top: -10px;*/
-  animation-name: catCycle;
-  animation-duration: calc(1s / 2);
-  width: 36px;
-  height: 24px;
-}
-@keyframes catCycle {
-  0%,
-  100% {
-    margin-top: -10px;
+  .cat-middle {
+    left: 50% !important;
   }
-  33.3% {
-    margin-top: -9px;
+
+  .cat-right {
+    left: 80% !important;
   }
-}
-.poptart {
-  left: 0;
-  top: 0;
-  width: 21px;
-  height: 18px;
-  background-image: linear-gradient(to right, #f39 0%, #f39 100%),
+
+  .overlay--quarter {
+    left: -12vw !important;
+  }
+
+  .cat {
+    /*top: 50%;*/
+    /*left: 50%;*/
+    /*margin-left: -10px;*/
+    /*margin-top: -10px;*/
+    animation-name: catCycle;
+    animation-duration: calc(1s / 2);
+    width: 36px;
+    height: 24px;
+  }
+  @keyframes catCycle {
+    0%,
+    100% {
+      margin-top: -8px;
+    }
+    33.3% {
+      margin-top: -6px;
+    }
+  }
+  .poptart {
+    left: 0;
+    top: 0;
+    width: 21px;
+    height: 18px;
+    background-image: linear-gradient(to right, #f39 0%, #f39 100%),
     linear-gradient(to right, #f39 0%, #f39 100%),
     linear-gradient(to right, #f39 0%, #f39 100%),
     linear-gradient(to right, #f39 0%, #f39 100%),
@@ -176,49 +169,49 @@ li {
       transparent calc(100% / 21 * 19),
       transparent 100%
     );
-  background-position:
-    9px 3px,
-    12px 3px,
-    4px 4px,
-    16px 5px,
-    8px 7px,
-    5px 9px,
-    9px 10px,
-    3px 11px,
-    7px 13px,
-    4px 14px,
-    11px 14px,
-    0 4px,
-    0 3px,
-    0 2px,
-    0 2px,
-    0 1px,
-    0 0;
-  background-size:
-    1px 1px,
-    1px 1px,
-    1px 1px,
-    1px 1px,
-    1px 1px,
-    1px 1px,
-    1px 1px,
-    1px 1px,
-    1px 1px,
-    1px 1px,
-    1px 1px,
-    100% 10px,
-    100% 12px,
-    100% 14px,
-    100% 14px,
-    100% 16px,
-    100% 100%;
-}
-.head {
-  left: 10px;
-  top: 5px;
-  width: 16px;
-  height: 13px;
-  background-image: linear-gradient(
+    background-position:
+      9px 3px,
+      12px 3px,
+      4px 4px,
+      16px 5px,
+      8px 7px,
+      5px 9px,
+      9px 10px,
+      3px 11px,
+      7px 13px,
+      4px 14px,
+      11px 14px,
+      0 4px,
+      0 3px,
+      0 2px,
+      0 2px,
+      0 1px,
+      0 0;
+    background-size:
+      1px 1px,
+      1px 1px,
+      1px 1px,
+      1px 1px,
+      1px 1px,
+      1px 1px,
+      1px 1px,
+      1px 1px,
+      1px 1px,
+      1px 1px,
+      1px 1px,
+      100% 10px,
+      100% 12px,
+      100% 14px,
+      100% 14px,
+      100% 16px,
+      100% 100%;
+  }
+  .head {
+    left: 10px;
+    top: 5px;
+    width: 16px;
+    height: 13px;
+    background-image: linear-gradient(
       to right,
       transparent 0%,
       transparent calc(100% / 16 * 2),
@@ -435,58 +428,58 @@ li {
       transparent calc(100% / 16 * 13),
       transparent 100%
     );
-  background-position:
-    0 0,
-    0 1px,
-    0 2px,
-    0 3px,
-    0 4px,
-    0 5px,
-    0 6px,
-    0 7px,
-    0 8px,
-    0 9px,
-    0 10px,
-    0 11px,
-    0 12px;
-  background-size: 100% 1px;
-  background-repeat: no-repeat;
-  animation-name: headCycle;
-  animation-duration: calc(1s / 2);
-}
-@keyframes headCycle {
-  0%,
-  100% {
-    margin-left: 0;
-    margin-top: 0;
+    background-position:
+      0 0,
+      0 1px,
+      0 2px,
+      0 3px,
+      0 4px,
+      0 5px,
+      0 6px,
+      0 7px,
+      0 8px,
+      0 9px,
+      0 10px,
+      0 11px,
+      0 12px;
+    background-size: 100% 1px;
+    background-repeat: no-repeat;
+    animation-name: headCycle;
+    animation-duration: calc(1s / 2);
   }
-  16.7% {
-    margin-left: 1px;
-    margin-top: 0;
+  @keyframes headCycle {
+    0%,
+    100% {
+      margin-left: 0;
+      margin-top: 0;
+    }
+    16.7% {
+      margin-left: 1px;
+      margin-top: 0;
+    }
+    66.7% {
+      margin-left: 0;
+      margin-top: 0;
+    }
+    83.3% {
+      margin-left: 0;
+      margin-top: -1px;
+    }
   }
-  66.7% {
-    margin-left: 0;
-    margin-top: 0;
+  .feet {
+    width: 24px;
+    height: 5px;
+    overflow: hidden;
+    left: -2px;
+    top: 15px;
+    animation-name: feetCycle;
+    animation-duration: calc(1s / 2);
   }
-  83.3% {
-    margin-left: 0;
-    margin-top: -1px;
-  }
-}
-.feet {
-  width: 24px;
-  height: 5px;
-  overflow: hidden;
-  left: -2px;
-  top: 15px;
-  animation-name: feetCycle;
-  animation-duration: calc(1s / 2);
-}
-.feet .sprite {
-  top: 0;
-  width: 24px;
-  height: 15px;
-  background-image: linear-gradient(
+  .feet .sprite {
+    top: 0;
+    width: 24px;
+    height: 15px;
+    background-image: linear-gradient(
       to right,
       transparent 0%,
       transparent calc(100% / 24 * 2),
@@ -797,70 +790,70 @@ li {
       black calc(100% / 24 * 21),
       black 100%
     );
-  background-position:
-    0 0,
-    0 1px,
-    0 2px,
-    0 3px,
-    0 4px,
-    0 5px,
-    0 6px,
-    0 7px,
-    0 8px,
-    0 9px,
-    0 10px,
-    0 11px,
-    0 12px,
-    0 13px,
-    0 14px;
-  background-size: 100% 1px;
-  background-repeat: no-repeat;
-  animation-name: feetSpriteCycle;
-  animation-duration: calc(1s / 2);
-}
-@keyframes feetCycle {
-  0%,
-  100% {
-    margin-left: 0;
+    background-position:
+      0 0,
+      0 1px,
+      0 2px,
+      0 3px,
+      0 4px,
+      0 5px,
+      0 6px,
+      0 7px,
+      0 8px,
+      0 9px,
+      0 10px,
+      0 11px,
+      0 12px,
+      0 13px,
+      0 14px;
+    background-size: 100% 1px;
+    background-repeat: no-repeat;
+    animation-name: feetSpriteCycle;
+    animation-duration: calc(1s / 2);
   }
-  16.7% {
-    margin-left: 1px;
+  @keyframes feetCycle {
+    0%,
+    100% {
+      margin-left: 0;
+    }
+    16.7% {
+      margin-left: 1px;
+    }
+    33.3% {
+      margin-left: 2px;
+    }
+    50% {
+      margin-left: 1px;
+    }
+    66.7% {
+      margin-left: -1px;
+    }
   }
-  33.3% {
-    margin-left: 2px;
+  @keyframes feetSpriteCycle {
+    0%,
+    100% {
+      top: 0;
+    }
+    16.7% {
+      top: -5px;
+    }
+    83.3% {
+      top: -10px;
+    }
   }
-  50% {
-    margin-left: 1px;
+  .tail {
+    width: 7px;
+    height: 7px;
+    overflow: hidden;
+    left: -7px;
+    top: 7px;
+    animation-name: tailCycle;
+    animation-duration: calc(1s / 2);
   }
-  66.7% {
-    margin-left: -1px;
-  }
-}
-@keyframes feetSpriteCycle {
-  0%,
-  100% {
-    top: 0;
-  }
-  16.7% {
-    top: -5px;
-  }
-  83.3% {
-    top: -10px;
-  }
-}
-.tail {
-  width: 7px;
-  height: 7px;
-  overflow: hidden;
-  left: -7px;
-  top: 7px;
-  animation-name: tailCycle;
-  animation-duration: calc(1s / 2);
-}
-.tail .sprite {
-  width: 7px;
-  height: 35px;
-  background-image: linear-gradient(
+  .tail .sprite {
+    width: 7px;
+    height: 35px;
+    background-image: linear-gradient(
       to right,
       transparent 0%,
       transparent calc(100% / 7),
@@ -1135,94 +1128,96 @@ li {
       black calc(100% / 7 * 5),
       black 100%
     );
-  background-position:
-    0 0,
-    0 1px,
-    0 2px,
-    0 3px,
-    0 4px,
-    0 5px,
-    0 6px,
-    0 7px,
-    0 8px,
-    0 9px,
-    0 10px,
-    0 11px,
-    0 12px,
-    0 13px,
-    0 14px,
-    0 15px,
-    0 16px,
-    0 17px,
-    0 18px,
-    0 19px,
-    0 20px,
-    0 21px,
-    0 22px,
-    0 23px,
-    0 24px,
-    0 25px,
-    0 26px,
-    0 27px,
-    0 28px,
-    0 29px,
-    0 30px,
-    0 31px,
-    0 32px,
-    0 33px,
-    0 34px;
-  background-size: 100% 1px;
-  background-repeat: no-repeat;
-  animation-name: tailSpriteCycle;
-  animation-duration: calc(1s / 2);
-}
-@keyframes tailCycle {
-  0%,
-  100% {
-    margin-top: 0;
+    background-position:
+      0 0,
+      0 1px,
+      0 2px,
+      0 3px,
+      0 4px,
+      0 5px,
+      0 6px,
+      0 7px,
+      0 8px,
+      0 9px,
+      0 10px,
+      0 11px,
+      0 12px,
+      0 13px,
+      0 14px,
+      0 15px,
+      0 16px,
+      0 17px,
+      0 18px,
+      0 19px,
+      0 20px,
+      0 21px,
+      0 22px,
+      0 23px,
+      0 24px,
+      0 25px,
+      0 26px,
+      0 27px,
+      0 28px,
+      0 29px,
+      0 30px,
+      0 31px,
+      0 32px,
+      0 33px,
+      0 34px;
+    background-size: 100% 1px;
+    background-repeat: no-repeat;
+    animation-name: tailSpriteCycle;
+    animation-duration: calc(1s / 2);
   }
-  33.3% {
-    margin-top: 1px;
+  @keyframes tailCycle {
+    0%,
+    100% {
+      margin-top: 0;
+    }
+    33.3% {
+      margin-top: 1px;
+    }
+    50% {
+      margin-top: 2px;
+    }
+    66.7% {
+      margin-top: -1px;
+    }
   }
-  50% {
-    margin-top: 2px;
+  @keyframes tailSpriteCycle {
+    0%,
+    83.3% {
+      margin-top: 0;
+    }
+    16.7% {
+      margin-top: -7px;
+    }
+    33.3% {
+      margin-top: -14px;
+    }
+    50% {
+      margin-top: -21px;
+    }
+    66.7% {
+      margin-top: -28px;
+    }
   }
-  66.7% {
-    margin-top: -1px;
+  .rainbow {
+    opacity: 1;
+    left: -100vw;
+    right: 50%;
+    top: 50%;
+    margin-top: -9px;
+    height: 19px;
+    overflow: hidden;
+    transition: opacity 500ms ease-in 100ms;
   }
-}
-@keyframes tailSpriteCycle {
-  0%,
-  83.3% {
-    margin-top: 0;
-  }
-  16.7% {
-    margin-top: -7px;
-  }
-  33.3% {
-    margin-top: -14px;
-  }
-  50% {
-    margin-top: -21px;
-  }
-  66.7% {
-    margin-top: -28px;
-  }
-}
-.rainbow {
-  left: -100vw;
-  right: 50%;
-  top: 50%;
-  margin-top: -9px;
-  height: 19px;
-  overflow: hidden;
-}
-.rainbow .sprite {
-  left: 0;
-  right: 8px;
-  top: 0;
-  bottom: 0;
-  background-image: linear-gradient(
+  .rainbow .sprite {
+    left: 0;
+    right: 8px;
+    top: 0;
+    bottom: 0;
+    background-image: linear-gradient(
       to right,
       #f00 0%,
       #f00 50%,
@@ -1306,248 +1301,47 @@ li {
       #63f 50%,
       #63f 100%
     );
-  background-position:
-    0 0,
-    0 1px,
-    0 3px,
-    0 4px,
-    0 6px,
-    0 7px,
-    0 9px,
-    0 10px,
-    0 12px,
-    0 13px,
-    0 15px,
-    0 16px;
-  background-size: 16px 3px;
-  background-repeat: repeat-x;
-  animation-name: rainbowCycle;
-  animation-duration: 1s;
-}
-@keyframes rainbowCycle {
-  0%,
-  33.3%,
-  66.7%,
-  100% {
-    left: 0;
+    background-position:
+      0 0,
+      0 1px,
+      0 3px,
+      0 4px,
+      0 6px,
+      0 7px,
+      0 9px,
+      0 10px,
+      0 12px,
+      0 13px,
+      0 15px,
+      0 16px;
+    background-size: 16px 3px;
+    background-repeat: repeat-x;
+    animation-name: rainbowCycle;
+    animation-duration: 1s;
   }
-  16.7%,
-  50%,
-  83.3% {
-    left: -9px;
+  @keyframes rainbowCycle {
+    0%,
+    33.3%,
+    66.7%,
+    100% {
+      left: 0;
+    }
+    16.7%,
+    50%,
+    83.3% {
+      left: -9px;
+    }
   }
-}
-/* this prevents the rainbow from peeking out from under the cat's back foot. */
-.rainbow .sprite:after {
-  content: '';
-  position: absolute;
-  bottom: 0;
-  right: 0;
-  width: 1.5px;
-  height: 1.5px;
-  background: #036;
-}
-.stars li {
-  width: 47px;
-  height: 7px;
-  overflow: hidden;
-  margin-left: -4px;
-}
-.stars li i {
-  display: block;
-  width: 47px;
-  height: 49px;
-  top: -42px;
-  background-image: linear-gradient(to right, white 0%, white 100%),
-    linear-gradient(to right, white 0%, white 100%),
-    linear-gradient(to right, white 0%, white 100%),
-    linear-gradient(to right, white 0%, white 100%),
-    linear-gradient(to right, white 0%, white 100%),
-    linear-gradient(to right, white 0%, white 100%),
-    linear-gradient(to right, white 0%, white 100%),
-    linear-gradient(to right, white 0%, white 100%),
-    linear-gradient(to right, white 0%, white 100%),
-    linear-gradient(to right, white 0%, white 100%),
-    linear-gradient(to right, white 0%, white 100%),
-    linear-gradient(to right, white 0%, white 100%),
-    linear-gradient(to right, white 0%, white 100%),
-    linear-gradient(to right, white 0%, white 100%),
-    linear-gradient(to right, white 0%, white 100%),
-    linear-gradient(to right, white 0%, white 100%),
-    linear-gradient(to right, white 0%, white 100%),
-    linear-gradient(to right, white 0%, white 100%),
-    linear-gradient(to right, white 0%, white 100%),
-    linear-gradient(to right, white 0%, white 100%),
-    linear-gradient(to right, white 0%, white 100%),
-    linear-gradient(to right, white 0%, white 100%),
-    linear-gradient(to right, white 0%, white 100%),
-    linear-gradient(to right, white 0%, white 100%),
-    linear-gradient(to right, white 0%, white 100%),
-    linear-gradient(to right, white 0%, white 100%);
-  background-position:
-    43px 3px,
-    35px 9px,
-    34px 10px,
-    36px 10px,
-    35px 11px,
-    27px 15px,
-    25px 17px,
-    28px 17px,
-    27px 18px,
-    19px 21px,
-    16px 24px,
-    19px 24px,
-    21px 24px,
-    19px 26px,
-    11px 28px,
-    9px 29px,
-    13px 29px,
-    8px 31px,
-    14px 31px,
-    9px 33px,
-    13px 33px,
-    11px 34px,
-    3px 35px,
-    0 38px,
-    6px 38px,
-    3px 41px;
-  background-size:
-    1px 1px,
-    1px 1px,
-    1px 1px,
-    1px 1px,
-    1px 1px,
-    1px 2px,
-    2px 1px,
-    2px 1px,
-    1px 2px,
-    1px 2px,
-    2px 1px,
-    1px 1px,
-    2px 1px,
-    1px 2px,
-    1px 1px,
-    1px 1px,
-    1px 1px,
-    1px 1px,
-    1px 1px,
-    1px 1px,
-    1px 1px,
-    1px 1px,
-    1px 1px,
-    1px 1px,
-    1px 1px,
-    1px 1px;
-  background-repeat: no-repeat;
-  animation-name: starSpriteCycle;
-  animation-duration: 1s;
-}
-@keyframes starSpriteCycle {
-  0%,
-  100% {
-    top: -42px;
+  /* this prevents the rainbow from peeking out from under the cat's back foot. */
+  .rainbow .sprite:after {
+    content: '';
+    position: absolute;
+    bottom: 0;
+    right: 0;
+    width: 1.5px;
+    height: 1.5px;
+    background: #036;
   }
-  50% {
-    top: 0;
-  }
-  58.3% {
-    top: -7px;
-  }
-  66.7% {
-    top: -14px;
-  }
-  75% {
-    top: -21px;
-  }
-  83.3% {
-    top: -28px;
-  }
-  91.7% {
-    top: -35px;
-  }
-}
-.stars li:nth-child(1) {
-  left: 66%;
-  top: 80%;
-}
-.stars li:nth-child(2) {
-  left: 70%;
-  top: 54%;
-}
-.stars li:nth-child(2) i {
-  animation-delay: calc(1s / 12);
-}
-.stars li:nth-child(3) {
-  left: 4%;
-  top: 31%;
-}
-.stars li:nth-child(3) i {
-  animation-delay: calc(1s / 12 * 2);
-}
-.stars li:nth-child(4) {
-  left: 49%;
-  top: 12%;
-}
-.stars li:nth-child(4) i {
-  animation-delay: calc(1s / 12 * 3);
-}
-.stars li:nth-child(5) {
-  left: 35%;
-  top: 30%;
-}
-.stars li:nth-child(5) i {
-  animation-delay: calc(1s / 12 * 4);
-}
-.stars li:nth-child(6) {
-  left: 48%;
-  top: 29%;
-}
-.stars li:nth-child(6) i {
-  animation-delay: calc(1s / 12 * 5);
-}
-.stars li:nth-child(7) {
-  left: 22%;
-  top: 70%;
-}
-.stars li:nth-child(7) i {
-  animation-delay: calc(1s / 12 * 8);
-}
-.stars li:nth-child(8) {
-  left: 56%;
-  top: 67%;
-}
-.stars li:nth-child(8) i {
-  animation-delay: calc(1s / 12 * 7);
-}
-.stars li:nth-child(9) {
-  left: 49%;
-  top: 12%;
-}
-.stars li:nth-child(9) i {
-  animation-delay: calc(1s / 12 * 8);
-}
-.stars li:nth-child(10) {
-  left: 9%;
-  top: 92%;
-}
-.stars li:nth-child(10) i {
-  animation-delay: calc(1s / 12 * 9);
-}
-.stars li:nth-child(11) {
-  left: 44%;
-  top: 20%;
-}
-.stars li:nth-child(11) i {
-  animation-delay: calc(1s / 12 * 10);
-}
-.stars li:nth-child(12) {
-  left: 6%;
-  top: 19%;
-}
-.stars li:nth-child(12) i {
-  animation-delay: calc(1s / 12 * 11);
-}
-
 `
 
 export default NyanCatStyles
